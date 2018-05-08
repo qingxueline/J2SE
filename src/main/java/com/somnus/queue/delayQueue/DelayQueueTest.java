057abe2a754a06f6cf0131eac4c67d7c3af47809
lyl <251409016@qq.com>
2018-05-08 14:50:30 +0800

    .

--- src/main/java/com/somnus/queue/delayQueue/DelayQueueTest.java
+++ src/main/java/com/somnus/queue/delayQueue/DelayQueueTest.java
@@ -5,11 +5,11 @@ package com.somnus.queue.delayQueue;
  */
 public class DelayQueueTest {
 
-    private static Long ACTIVE_TIME = 1000 * 30L;//延迟执行时间，6秒后执行
+    private static Long ACTIVE_TIME = 1000*10L;//延迟执行时间，6秒后执行
 
     public static void main(String[] args) {
         TaskQueueDaemonThread.getInstance().init();
-        for (Long i = 0L; i <= 100000000000L; i++) {
+        for (Long i = 0L; i <= 1000000000L; i++) {
             System.out.println("产生问题对象：" + i);
             TaskQueueDaemonThread.getInstance().put(ACTIVE_TIME, new Order(String.valueOf(i)));
         }
