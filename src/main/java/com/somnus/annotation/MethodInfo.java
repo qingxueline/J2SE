6f78b980c724c5ea7dd07979570d3ff25cb25ade
lyl <251409016@qq.com>
2019-03-18 15:37:41 +0800

    添加二维码生成工具类

--- src/main/java/com/somnus/annotation/MethodInfo.java
+++ src/main/java/com/somnus/annotation/MethodInfo.java
@@ -11,7 +11,15 @@ import java.lang.annotation.RetentionPolicy;
  * 注解方法不能有参数
  * 注解方法的返回类型局限于原始类型，字符串，枚举，注解，或以上类型构成的数组
  * 注解方法可以包含默认值
- * 注解可以包含与其绑定的元注解，元注解为注解提供信息，有四种元注解类型：@Documented 、@Target、@Inherited、@Retention 
+ * 注解可以包含与其绑定的元注解，元注解为注解提供信息，有四种元注解类型：@Documented 、@Target、@Inherited、@Retention
+ *
+ * 1.CONSTRUCTOR:用于描述构造器
+ * 2.FIELD:用于描述域
+ * 3.LOCAL_VARIABLE:用于描述局部变量
+ * 4.METHOD:用于描述方法
+ * 5.PACKAGE:用于描述包
+ * 6.PARAMETER:用于描述参数
+ * 7.TYPE:用于描述类、接口(包括注解类型) 或enum声明
  */
 
 @Documented
