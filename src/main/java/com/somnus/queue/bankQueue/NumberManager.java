eb713f623404f32359d53af2a3fb061010e4734f
lyl <251409016@qq.com>
2020-08-23 21:42:18 +0800

    代码修改

--- src/main/java/com/somnus/queue/bankQueue/NumberManager.java
+++ src/main/java/com/somnus/queue/bankQueue/NumberManager.java
@@ -6,29 +6,30 @@ import java.util.List;
 /**
  * 号码机
  */
-public class NumberManager{
-	private int lastNumber = 0;
-	private List<Integer> queueNumbers = new ArrayList<Integer>();
+public class NumberManager {
+    private int lastNumber = 0;
+    private List<Integer> queueNumbers = new ArrayList<Integer>();
 
-	/**
-	 * 产生号码，客户触发
-	 * @return
-	 */
-	public synchronized Integer generateNewNumber(){
-		queueNumbers.add(++lastNumber);
-		return lastNumber;
-	}
+    /**
+     * synchronized堵塞的方法，产生号码，客户触发
+     *
+     * @return
+     */
+    public synchronized Integer generateNewNumber() {
+        queueNumbers.add(++lastNumber);
+        return lastNumber;
+    }
 
-	/**
-	 * 取走号码，窗口触发
-	 * @return
-	 */
-	public synchronized Integer fetchNumber(){
-		if (queueNumbers.size() > 0){
-			return (Integer) queueNumbers.remove(0);
-		}
-		else{
-			return null;
-		}
-	}
+    /**
+     * synchronized堵塞的方法，取走号码，窗口触发
+     *
+     * @return
+     */
+    public synchronized Integer fetchNumber() {
+        if (queueNumbers.size() > 0) {
+            return (Integer) queueNumbers.remove(0);
+        } else {
+            return null;
+        }
+    }
 }
